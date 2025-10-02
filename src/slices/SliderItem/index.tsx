7617584fe36e4ca8b3da637ac5b12c696163dc7d
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-10-02 11:44:28 +0200

    update and fixes

--- src/slices/SliderItem/index.tsx
+++ src/slices/SliderItem/index.tsx
@@ -1,59 +1,55 @@
-'use client'
+"use client";
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
 import styles from "./index.module.scss";
 
-/**
- * Props for `SliderItem`.
- */
 export type SliderItemProps = SliceComponentProps<Content.SliderItemSlice>;
 
-/**
- * Component for "SliderItem" Slices.
- */
 const SliderItem: FC<SliderItemProps> = ({ slice }) => {
-  const settings = {
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "20%",
-    adaptiveHeight: true,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false,
-          centerPadding: "0px",
-          arrows: false,
-        },
-      },
-    ],
-  };
-
-
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={styles.slider}
     >
-      <Slider {...settings}>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        spaceBetween={20}
+        centeredSlides={true}         // enables "peek"
+        slidesOffsetBefore={0}
+        slidesOffsetAfter={0}
+        breakpoints={{
+          // Mobile (< 900px): full width
+          0: {
+            slidesPerView: 1,
+            centeredSlides: false,    // disable peek
+            spaceBetween: 0,
+          },
+          // Desktop (>= 900px): enable peek
+          900: {
+            slidesPerView: 2,
+            centeredSlides: true,     // keep slide centered
+            spaceBetween: 20,         // spacing between slides
+          },
+        }}
+        navigation={false}
+        pagination={{ clickable: true }}
+      >
         {slice.primary.images.map((item, idx) => (
-          <div key={idx} className={styles.slide}>
+          <SwiperSlide key={idx} className={styles.slide}>
             <PrismicNextImage field={item.image} />
             {item.caption && <PrismicRichText field={item.caption} />}
-          </div>
+          </SwiperSlide>
         ))}
-      </Slider>
+      </Swiper>
     </section>
   );
 };
