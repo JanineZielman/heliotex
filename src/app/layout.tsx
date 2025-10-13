056416a8c184bca591e4ca04f4df96a09db883f4
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-10-13 11:24:08 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
